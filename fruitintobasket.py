f8f7b5f15de167e7181a2fee0670eca81cdba50f
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-05-03 21:31:35 +0530

    update

---
+++ fruitintobasket.py
@@ -0,0 +1,22 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        n = len(fruits)
+        max_len= 0
+        low=0
+        basket ={}
+        
+
+        for high in range(n):
+            basket[fruits[high]]= basket.get(fruits[high],0)+1
+            
+
+            while len(basket)>2:
+                basket[fruits[low]] -=1    
+                if basket[fruits[low]]==0:
+                    del basket[fruits[low]]
+                low+=1
+            if len(basket)==2:
+                max_len= max(max_len,high-low+1)
+        return max_len
+
+
