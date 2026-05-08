f7238fd1d0ed5bb98a5e8e6a245141a7094447a5
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-05-08 15:51:41 +0530

    kadane max sub array

---
+++ maxSubArray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        i = 0
+        best_ending = nums[0]
+        ans = nums[0]
+
+        for i in range(1,n):
+            v1 = best_ending+nums[i]
+            v2 = nums[i]
+            best_ending = max(v1,v2)
+            ans = max(ans , best_ending)
+        return ans
+        
