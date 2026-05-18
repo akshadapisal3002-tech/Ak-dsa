48121f331e8e41134ae3c71cdd3366c9f844e429
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-05-18 13:26:39 +0530

    Reverse nodes in kth group

---
+++ ReverseNodeinkth.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseKGroup(self, head, k): 
+        if not head:
+            return None
+        
+        left = head
+        right = None
+        res = None
+        prevleft = None
+        size = k # k = 2 for swap pairs
+
+        while True:
+            right = left
+            # Walk k-1 steps to find right
+            for i in range(size - 1):
+                if not right:
+                    break
+                right = right.next
+
+            if right:  # left and right mil chuke hai
+                nextleft = right.next
+                self.reverse(left, size)
+                if prevleft:
+                    prevleft.next = right
+                prevleft = left
+                if not res:
+                    res = right
+                left = nextleft
+
+            else:  # khatam hai sb
+                if prevleft:
+                    prevleft.next = left
+                if not res:
+                    res = left
+                break
+
+        return res
+
+    def reverse(self, head, size):
+        prev = None
+        curr = head
+        for _ in range(size):
+            if not curr:
+                break
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
