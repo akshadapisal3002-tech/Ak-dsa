a30f316800fd0e35a81876995b07deda42912a4e
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-05-20 14:35:21 +0530

    Peak Index in a mountain Arraybinary search

---
+++ peakIndexMoutain.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def peakIndexInMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        low = 0
+        high = len(arr)-1
+        res =-1
+        while(low <= high):
+            guess = (low+high)//2
+            if arr[guess]< arr[guess+1]:
+                low = guess+1
+            else:
+                res = guess
+                high = guess -1
+
+        return res
