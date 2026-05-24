3f3d90a7df2cab98f36029a42582edc1370875b1
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-05-24 15:29:53 +0530

    Last stone weight in  heap

---
+++ LastStone.py
@@ -0,0 +1,15 @@
+import heapq
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        heap =[-s for s in stones]
+        heapq.heapify(heap)
+        while len(heap)>1:
+            y = -heapq.heappop(heap)
+            x = -heapq.heappop(heap)
+            if x!= y:
+                heapq.heappush(heap,-(y-x))
+        return - heap[0] if heap else 0
