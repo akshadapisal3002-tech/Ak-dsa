dfb5f80437a40b05f4e89827d710051fcc69cc5d
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-05-27 19:22:41 +0530

    level Orderby Tree

---
+++ levelOrder.py
@@ -0,0 +1,26 @@
+from collections import deque
+from typing import Optional,List
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        result = []
+        q = deque()
+        q.append(root)
+        #meanas while q is not empty
+        while q:
+            level_size = len(q)
+            temp = []
+
+            while level_size > 0 :
+                t = q.popleft()
+                temp.append(t.val)
+
+                if t.left:
+                    q.append(t.left)
+                if t.right:
+                    q.append(t.right)
+                level_size -=1
+
+            result.append(temp)
+        return result
