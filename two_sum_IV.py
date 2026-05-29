9de0df79fc5df29fc50c0666d8b63a46f6079027
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-05-29 16:05:04 +0530

    Two sum IV in  binary search  tree

---
+++ two_sum_IV.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        nums = []
+        def inorder(node):
+            if node is None :
+                return 
+            inorder(node.left)
+            nums.append(node.val)
+            inorder(node.right)
+
+        inorder(root)
+
+        left = 0
+        right = len(nums)-1
+        while left<= right:
+            target = nums[left] + nums[right]
+            if target == k:
+                return True
+            elif target < k:
+                left +=1
+            else:
+                right -=1
+        return False
