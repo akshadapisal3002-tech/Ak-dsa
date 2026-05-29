1bb2465cabd407d8997bc69808a9c5477fbe1557
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-05-29 14:42:28 +0530

    lowest common ancestor of  binary search  tree

---
+++ Lca_Bst.py
@@ -0,0 +1,26 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        ans = None
+        if p.val > q.val:
+            p, q = q, p 
+
+        def tree(node,p,q):
+            nonlocal ans
+
+            if node is None:
+                return 
+
+            if p == node or q == node:
+                ans = node
+                return
+            if node.val < p.val:
+                tree(node.right,p,q)
+            elif node.val > q.val:
+                tree(node.left , p ,q)
+            else:
+                ans = node
+                return
+        tree(root , p , q)
+        return ans
+        
+        
