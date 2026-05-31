af7af17c60d473b8f3f9139cd27edd2831ea6e15
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-05-31 14:28:24 +0530

    print adjaceny List in Graph

---
+++ adjacenyList.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def printGraph(self, V : int, edges : List[List[int]]) -> List[List[int]]:
+        adj = [[]for _ in range(V)]
+        
+        for u , v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+            
+        return adj
+ 
+       
