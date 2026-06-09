8854f014101e583ccc173fe341f77dc5cf73eea6
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-06-09 15:39:56 +0530

    practice 15

---
+++ practice15.py
@@ -0,0 +1,16 @@
+class Solution:
+    def Longest_subtring(self , s :str)-> int:
+        n = len(s)
+        low = 0 
+        max_len = 0
+        char_set = set()
+
+        for high in range(n):
+            while s[high] in char_set:
+                char_set.remove(s[low])
+                low+=1
+
+            char_set.add(s[high])
+            max_len = max(max_len,high-low+1)
+        return max_len
+            
