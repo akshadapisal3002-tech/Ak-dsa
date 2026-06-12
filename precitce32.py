91a78f461e0870ea8fb6c0c0130d6d3dae43c7a1
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-06-12 13:03:58 +0530

    prcatice 32

---
+++ precitce32.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxAbsoluteSum(self, nums: List[int]) -> int:
+        best_positive= 0
+        best_negative =0
+
+        positive_sum = 0
+        negative_sum =0 
+
+        for num in nums (n):
+            positive_sum = max(positive_sum+nums,nums)
+            best_positive = max(best_positive,positive_sum)
+
+            negative_sum = max(negative_sum,nums)
+            best_negative = max(best_negative,negative_sum)
+
+            ans = max(best_positive,best_negative)
+        return ans
