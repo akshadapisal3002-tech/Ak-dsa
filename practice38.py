26dbed5f4ed75a2c0e546cd2a747923d4b757359
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-06-15 13:45:58 +0530

    prcatice 38

---
+++ practice38.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isIntersect(self, intervals):
+        n = len(intervals)
+        intervals.sort()
+        start1 = intervals [0][0]
+        end1 = intervals[0][1]
+        for i in range(1,n):
+            start2 = intervals[i][0]
+            end2 = intervals[i][1]
+            if end1 >= start2:
+                return True
+            start1 = start2
+            end1 = end2
+        return False 
