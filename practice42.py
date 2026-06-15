851cec4c5c17e3198cd02808d74c79e1520fdaab
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-06-15 14:09:39 +0530

    prcatice 42

---
+++ practice42.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def reverseString(self, s):
+        stack = []
+        for ch in s:
+            stack.append(ch)
+        for i in range(len(s)):
+            s[i]=stack.pop()
+        return stack
