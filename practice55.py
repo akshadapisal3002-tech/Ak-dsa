3fea6f9f22c6c4bcb6a1fd9a33d9685b275b187e
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-06-19 13:54:21 +0530

    prcatice 55

---
+++ practice55.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        for _ in range(left-1):
+            prev = prev.next
+        curr = prev.next
+        for _ in range(right-left):
+            nxt = curr.next
+            curr.next = nxt.next
+            nxt.next = prev.next
+            prev.next = nxt
+        return dummy.next
