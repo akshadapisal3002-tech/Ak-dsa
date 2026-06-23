dddf559d6733cae384c62060e6df652ac6645aa5
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-06-23 13:36:53 +0530

    prcatice 76

---
+++ practice76.py
@@ -0,0 +1,21 @@
+import heapq
+from collections import Counter
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        frq = Counter(tasks)
+        heap = [-t for t in frq.values()]
+        heapq.heapify(heap)
+
+        time =0
+        while heap:
+            cycle = n+1
+            temp = [] 
+
+            for _ in range(cycle):
+                if heap:
+                    temp.append(heapq.heappop(heap))
+            for f in temp:
+                if f+1<0:
+                    heapq.heappush(heap,f+1)
+                time += cycle if heap else len(temp)
+        return time
