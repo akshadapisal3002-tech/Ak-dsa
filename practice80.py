5f98b0e63b73c756dd7581e2ef68ce17a430a67a
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-06-24 13:24:41 +0530

    prcatice 80

---
+++ practice80.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSorted(self,arr,i= 0,n= None):
+        if n is None:
+            n = len(arr)
+        if i ==n or i ==n-1:
+            return True
+        if arr[i]>arr[i+1]:
+            return False
+        return self.isSorted(arr,i+1,n)
