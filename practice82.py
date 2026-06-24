dc35d33bea2e13f81d19fe11acc6b68ce3610650
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-06-24 13:48:41 +0530

    prcatice 82

---
+++ practice82.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res =[]
+        self.fun(0,0,n,"",res)
+        return res
+    def fun(self,open,close,n,temp,res):
+        if open ==n and close ==n:
+            res.append(temp)
+            return
+        if open < n:
+            self.fun(open+1,close,n,temp+'(',res)
+        if close <open:
+            self.fun(open,close+1,n,temp+')',res)
