a0f29c837a91c40c8f8e11eb3f95d14302e55137
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-06-25 13:17:33 +0530

    prcatice 87

---
+++ practice87.py
@@ -0,0 +1,11 @@
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        def tree(node):
+            if node is None:
+                return
+            result.append(node.val)
+            tree(node.left)
+            tree(node.right)
+        tree(node)
+        return result
