bec1699f012846bb65a3e0fc4ef34629f92b3875
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-06-27 13:44:29 +0530

    prcatice 96

---
+++ practice.96.py
@@ -0,0 +1,14 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res =0
+        def height(node):
+            nonlocal res
+            if node is None:
+                return 0
+            left = height(node.left)
+            right = height(node.right)
+            sum_of = left+right
+            res = max(res,sum_of)
+            return 1+max(left,right)
+        height(root)
+        return res
