2ab41473d377693f240ed8aec9f7df4db98cecff
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-06-29 16:56:48 +0530

    prcatice 99

---
+++ practice99.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], target: int) -> bool:
+        def path(node,target):
+            if node is None:
+                return False
+            target -= node.val
+            if node.left is None and node.right is None:
+                return target== 0
+            left = path(node.left,target)
+            right = path(node.right,target)
+            if left or right :
+                return True
+            return False
+        return path(root,target)
