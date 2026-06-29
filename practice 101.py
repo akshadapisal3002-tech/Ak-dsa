bcde95e334bd18eb1f859c45afc221f82bc1741d
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-06-29 17:08:18 +0530

    prcatice 101

---
+++ practice 101.py
@@ -0,0 +1,23 @@
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        result = []
+
+        def summ(node, target, diary):
+            if node is None:
+                return
+
+            target -= node.val
+            diary.append(node.val)
+
+            if node.left is None and node.right is None:
+                if target == 0:
+                    result.append(diary[:])
+                    diary.pop()
+                    return
+
+                summ(node.left, target, diary)      
+                summ(node.right, target, diary)     
+                diary.pop()                        
+
+            summ(root, targetSum, [])               
+            return result                           
