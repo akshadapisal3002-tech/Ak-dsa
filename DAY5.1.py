5112781f9601b5e89f7facace96cc4720f4a2ab6
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-07-23 20:05:37 +0530

    DAY5 Q2

---
+++ DAY5.1.py
@@ -0,0 +1,22 @@
+class Solution():
+    def MAx_subarray(self,arr,k):
+        n= len(arr)
+
+        if n<k:
+            return None
+        
+        low = 0
+        high = k-1
+
+        window_sum = 0
+        for i in range (low,high+1):
+            window_sum+=arr[i]
+
+        max_sum = window_sum
+        while high<n-1:
+            high+=1
+            window_sum += arr[high]
+            window_sum -=arr[low]
+            low+=1
+            max_sum= max(max_sum,window_sum)
+        return max_sum
---
+++ DAY5.2.py
@@ -0,0 +1,16 @@
+class Solution():
+    def longet_substring(self,s):
+        n = len(s)
+        low = 0
+        max_len = 0
+        char_set = set()
+
+        for high in range(n):
+            while s[high]in char_set:
+                char_set.remove(s[low])
+                low+=1
+            char_set.add(s[high])
+            max_len = max(max_len,high-low+1)
+        return max_len
+
+        
