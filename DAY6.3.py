f7d27ca4c0fde9576acda84336f40c405cb867af
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-07-26 19:13:14 +0530

    DAY6 Q3

---
+++ DAY6.3.py
@@ -0,0 +1,15 @@
+def max_product(nums):
+    n =len(nums)
+    i =0
+    best_ending_max=nums[0] 
+    best_ending_min = nums[0]
+
+    for i in range(1,n):
+        v1 = nums[i]
+        v2 = best_ending_max*nums[i]
+        v3= best_ending_min*nums[i]
+
+        best_ending_max= max(v1,v2,v3)
+        best_ending_min = min(v1,v2,v3)
+
+        ans = max(ans,best_ending_max,best_ending_min)
