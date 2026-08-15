bc90e27270a5952581c5392abdaa5512c8f926b3
akshadapisal3002-tech <akshadapisal3002@gmail.com>
2026-08-15 20:08:27 +0530

    DAY11 Q2

---
+++ DAY11.2.py
@@ -0,0 +1,11 @@
+class Solution():
+    def reverse(self,arr):
+        i = 0
+        j = len(arr)-1
+
+        while i < j:
+            arr[i],arr[j]=arr[j],arr[i]
+            i+=1
+            j-=1
+            
+        return arr
